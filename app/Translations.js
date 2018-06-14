b56f738d7a3f03cb8d06ce4c49c6684ced96fe0c
dhrubajitpc <dhrubajit1992@gmail.com>
2018-06-14 12:18:58 +0800

    add file with translations for both english and french

---
+++ app/Translations.js
@@ -0,0 +1,10 @@
+export default {
+  en: {
+    ln: 'EN',
+    lorem: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Maecenas lorem. Duis viverra diam non justo. Donec vitae arcu. Nulla est. Vivamus ac leo pretium faucibus. Aenean vel massa quis mauris vehicula lacinia. Vivamus ac leo pretium faucibus. Vivamus luctus egestas leo. Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Etiam neque.',
+  },
+  fr: {
+    ln: 'FR',
+    lorem: 'Duis bibendum, lectus ut viverra rhoncus, dolor nunc faucibus libero, eget facilisis enim ipsum id lacus. Duis pulvinar. Nullam sit amet magna in magna gravida vehicula. Morbi leo mi, nonummy eget tristique non, rhoncus non leo. Nullam at arcu a est sollicitudin euismod. Vivamus ac leo pretium faucibus. Sed ac dolor sit amet purus malesuada congue. Aenean placerat. Sed vel lectus. Donec odio tempus molestie, porttitor ut, iaculis quis, sem. Nulla turpis magna, cursus sit amet, suscipit a, interdum id, felis. Aliquam in lorem sit amet leo accumsan lacinia. Pellentesque pretium lectus id turpis',
+  }
+};
