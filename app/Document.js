58fb3c94c08845c1d07311081b2306c6edb06821
Dhrubajit <dhrubajit1992@gmail.com>
2018-06-16 10:18:39 +0600

    add function to check hasDefaultUserPref

--- app/Document.js
+++ app/Document.js
@@ -1,6 +1,7 @@
 import {Record} from 'immutable';
 import {Config} from './Config';
 import uuid from 'uuid';
+import isEqual from 'lodash.isequal';
 
 export default class Document extends Record({
   uuid: uuid.v4(),
@@ -9,9 +10,7 @@ export default class Document extends Record({
   metadata: Config.DEFAULT_METADATA,
   last_modified: null, // defined by the server
   last_modified_locally: null,
-  userPref: {
-    language: 'en'
-  },
+  userPref: Config.DEFAULT_USERPREF,
 }) {
 
   hasDefaultContent() {
@@ -30,7 +29,11 @@ export default class Document extends Record({
     return null === this.last_modified_locally;
   }
 
+  hasDefaultUserPref() {
+    return isEqual(Config.DEFAULT_USERPREF, this.userPref);
+  }
+
   isNew() {
-    return this.hasDefaultContent() && this.hasDefaultMetadata() && this.hasNeverBeenSync() && this.hasNoLocalChanges();
+    return this.hasDefaultUserPref() && this.hasDefaultContent() && this.hasDefaultMetadata() && this.hasNeverBeenSync() && this.hasNoLocalChanges();
   }
 }
