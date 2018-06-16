af6eb1a0d5cf74230287c25bad547ecf408cf771
Dhrubajit <dhrubajit1992@gmail.com>
2018-06-16 11:55:57 +0600

    move react-intl to inside App.jsx

--- app/index.jsx
+++ app/index.jsx
@@ -2,17 +2,11 @@ import './scss/main.scss';
 
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { addLocaleData, IntlProvider } from 'react-intl';
-import en from 'react-intl/locale-data/en';
-import fr from 'react-intl/locale-data/fr';
 import App from './components/App.jsx';
-
 import { EventEmitter } from 'events';
 import localforage from 'localforage';
 import Store from './Store';
 import Controller from './Controller';
-
-import Translations from './Translations';
 
 const appElement = document.getElementById('app');
 const appVersion = appElement.getAttribute('data-app-version');
@@ -22,26 +16,13 @@ const events = new EventEmitter();
 const store = new Store('documents', events, apiEndpoint, localforage);
 const controller = new Controller({ store }, events);
 
-addLocaleData([...en, ...fr]);
-
-let locale = 
-  (navigator.languages && navigator.languages[0])
-  || navigator.language
-  || navigator.userLanguage
-  || 'en';
-
 require('offline-plugin/runtime').install();
 
 if((window.location.href + '').indexOf('?help') != -1){
   history.replaceState(null, 'zenika-resume', (window.location.href + '').split('?help')[0]);
 }
-if (window.location.search) {
-  locale = window.location.search.split('=')[1].slice(0, 5);
-}
 
 ReactDOM.render(
-  <IntlProvider locale={locale} messages={Translations[locale]}>
-    <App version={appVersion} controller={controller} />
-  </IntlProvider>,
+  <App version={appVersion} controller={controller} />,
   appElement
 );
