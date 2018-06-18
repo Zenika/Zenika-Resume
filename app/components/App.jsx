20639baa3cbd216e7d7dbb26fdd0bef655c5c61d
Julien COQUET <juliencoquet@Juliens-MacBook-Pro.local>
2018-06-18 16:28:51 +0800

    temporary commit, added translations

--- app/components/App.jsx
+++ app/components/App.jsx
@@ -9,7 +9,7 @@ import en from 'react-intl/locale-data/en';
 import fr from 'react-intl/locale-data/fr';
 import { Events } from '../Store';
 import Document from '../Document';
-import Translations from '../Translations';
+import Translations from './Translations';
 import debounce from 'lodash.debounce';
 import isEqual from 'lodash.isequal';
 
@@ -229,66 +229,65 @@ class App extends Component {
         >
         <div className={`layout ${viewMode}`} style={style}>
         <div className="reveal" id="help-modal" data-reveal>
-        <h1>Ouf je suis sauvé!</h1>
+        <h1><FormattedMessage id="phew"/ppwd></h1>
 
         <iframe width="640" height="360" src="https://www.youtube.com/embed/NFQaIcU54J4?rel=0&amp;showinfo=0"
           frameBorder="0" allowFullScreen
         ></iframe>
 
         <p className="dummyTest">
-        <FormattedMessage id="lorem" />
         </p>
-        <p className="lead">Impression en pdf</p>
+        <p className="lead"><FormattedMessage id="print"/></p>
         <p>
-        L'application a été testé avec Chrome. Il est possible de demander dans <br />
-        le navigateur l'impression de la page de maniere classique ou dans un PDF.<br />
+        <FormattedMessage id="the"/><br />
+        <FormattedMessage id="between"/><br />
         <br />
-        Pour que s'affiche correctement, il faut s'assurer de certains paramètres dans<br />
-        le panneau d'impression. Déplier les options en cliquant sur 'Plus de paramètres'<br />
-        puis dans la section Options, Graphiques d'arrière-plan doit être coché.<br />
-        Pour un meilleur effet, jouez avec les marges (Aucun, par exemple).
+        <FormattedMessage id="in"/><br />
+        <FormattedMessage id="the2"/><br />
+        <FormattedMessage id="then"/><br />
+        <FormattedMessage id="for"/>
         </p>
-        <p className="lead">Décrire une expérience</p>
+        <p className="lead"><FormattedMessage id="describe"/></p>
         <p>
-        Les experiences doivent être délimitées par des marquers pour définir<br />
-        les styles qui doivent être appliqués :<br />
+        <FormattedMessage id="experience"/><br />
+        <FormattedMessage id="styles"/><br />
         --section-start<br />
         --section-end<br />
         <br />
-        Entre ces deux marquers, le premier élément doit être un titre :<br />
-        # Votre role pour la mission<br />
+        <FormattedMessage id="between"/><br />
+        <FormattedMessage id="your"/><br />
         <br />
-        Le deuxiement élément doit être un titre de second niveau :<br />
-        ## Le nom du client<br />
+        <FormattedMessage id="the3"/><br />
+        <FormattedMessage id="the4"/><br />
         <br />
-        Le troisième élément doit être une date: <br />
-        ## Les dates de la mission <br />
+        <FormattedMessage id="the5"/><br />
+        <FormattedMessage id="the6"/><br />
         <br />
-        Pour créer une section avec un descriptif puis des points, il faut avoir<br />
-        deux niveaux de liste<br />
-        * La description de votre paragraphe<br />
-        &nbsp; &nbsp; * Un détail<br />
-        &nbsp; &nbsp; * puis un autre<br />
+        <FormattedMessage id="to"/><br />
+        <FormattedMessage id="two"/><br />
+        <FormattedMessage id="the7"/><br />
+        &nbsp; &nbsp; <FormattedMessage id="a"/><br />
+        &nbsp; &nbsp; <FormattedMessage id="then2"/><br />
         <br />
         </p>
 
-        <p className="lead">Déclaration des variables</p>
+        <p className="lead"><FormattedMessage id="variable"/></p>
         <p>
-        Au début du document, se situe une section entre '---'. Dans cette<br />
-        section il doit être définit un certain nombre de variables (nom prénom,<br />
-          role, expérience, ...). Les variables manquantes seront alors affiché dans<br />
-          le template avec [nomdevariable].<br />
-          Pour les listes il est possible d'ajouter autant d'élément que souhaité, il<br />
-          suffit d'incrémenter le nombre.<br /><br />
+        <FormattedMessage id="at"/><br />
+        <FormattedMessage id="section"/><br />
+        <FormattedMessage id="role"/><br />
+        <FormattedMessage id="the8"/><br />
+        <FormattedMessage id="for2"/><br />
+        <FormattedMessage id="you"/><br /><br />
           </p>
 
-          <p className="lead">Utilisation des images pour les expertises</p>
+          <p className="lead"><FormattedMessage id="using"/></p>
           <p>
-          Il existe un nombre limité d'icon pour illustrer les expertises. Pour les<br />
-          utiliser, il faut dans la liste d'expertise utiliser un marquer de la forme :<br />
+          <FormattedMessage id="there"/><br />
+          <FormattedMessage id="it"/><br />
           --expertise-archive.<br />
           <br />
-          Voici l'ensemble des marquers disponibles:<br />
+          <FormattedMessage id="here"/><br />
           --expertise-archive<br />
           --expertise-cloud<br />
           --expertise-file<br />
@@ -297,17 +296,17 @@ class App extends Component {
           --expertise-talk<br /><br />
           </p>
 
-          <p className="lead">Sauter à la page suivante</p>
+          <p className="lead"><FormattedMessage id="jump"/></p>
           <p>
-          Le marquer permet de sauter une page, c'est surtout utile pour définir<br />
-          ce qui doit être affiché sur la premiere page :<br />
+          <FormattedMessage id="this"/><br />
+          <FormattedMessage id="what"/><br />
           --break-page<br />
           </p>
 
-          <p className="lead">Astuce pour les expertises</p>
+          <p className="lead"><FormattedMessage id="tip"/></p>
           <p>
-          Afin de rajouter les expertises, il faut les mettre entre " ". <br />
-          Exemple: "Spring (Core, Batch), Java" <br />
+          <FormattedMessage id="in2"/> <br />
+          <FormattedMessage id="example"/><br />
           </p>
 
           <button className="close-button" data-close aria-label="Close modal" type="button" onClick={this.hideHelp}>
