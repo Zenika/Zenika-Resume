ddba9a9bbd6e4f25f435c461bffa1e9ddfd7f275
Ked57 <kedketh@gmail.com>
2019-08-30 14:39:57 +0200

    Add router

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { Router, RouteComponentProps } from "@reach/router"
 import "./App.css";
 import Home from "./components/Home";
 
 const App: React.FC = () => {
-  
-  return <Home></Home>
+  return <Router>
+            <RouterPage path="/" pageComponent={<Home></Home>}/>
+          </Router>
 };
 
 export default App;
+
+const RouterPage = (
+  props: { pageComponent: JSX.Element } & RouteComponentProps
+) => props.pageComponent;
