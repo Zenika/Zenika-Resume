ba65ba60e16b4355cdc3c864060d38da994dd2d4
Hugo Wood <hugo.wood@zenika.com>
2020-12-08 11:03:27 +0100

    🐛 fix metadata search
    Was using 'simple' config to analyze metadata but turns out using english and french works way better.

---
+++ .hasura/migrations/1607421626233_improve_metadata_search/down.sql
@@ -0,0 +1,9 @@
+alter table resume
+    drop column full_text_search_vector;
+
+alter table resume
+    add column full_text_search_vector tsvector
+        generated always as (
+            setweight(to_tsvector('simple', metadata), 'A') 
+            || setweight(to_tsvector_multilang(content), 'C')
+        ) stored;
