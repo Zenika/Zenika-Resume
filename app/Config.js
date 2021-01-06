65481fedf1d7f8d9c5056ca002b096964f9cb26f
Hugo Wood <hugo.wood@zenika.com>
2021-01-06 16:11:14 +0100

    💬 improve resume template
    Hopefully it guides newcomers a bit better

--- app/Config.js
+++ app/Config.js
@@ -14,88 +14,53 @@ export const Config = {
     },
   ],
   DEFAULT_METADATA: {
-    name: 'Williams',
-    firstname: 'Rory',
-    email: 'rory.williaws@zenika.box.com',
-    agency: 'Montreal',
+    name: 'Last-Name',
+    firstname: 'First-Name',
+    email: 'first-name.last-name@zenika.com',
+    agency: 'Your agency here (Lille, Lyon, Rennes, ...)',
     lang: 'fr',
-    experience: '2 ans d\'expérience',
-    description: `# Architecte – ScrumMaster - Développeur Front Senior
-rory.williaws@zenika.box.com`,
-    column1: `* Portfolio : bbc.in/1OCrHKk
+    experience: 'N ans d\'expérience',
+    description: `Your headline here`,
+    column1: `[Use this column to highlight skills, external profiles etc. This following is an example.]::
+* Portfolio : bbc.in/1OCrHKk
 * Exemples de code : codepen.io/phenax/pen/WQXerz`,
-    column2: `# **Formateur**
-  * Angular JS
-  * Angular 2
-  * Ecplise RCP`,
-    column3: `# **Conférencier**
-  * Devoxx
-  * Mix IT`,
+    column2: `[Use this column to highlight skills, external profiles etc. This following is an example.]::
+# **Formateur**
+* Angular JS
+* Angular 2
+* Ecplise RCP`,
+    column3: `[Use this column to highlight skills, external profiles etc. This following is an example.]::
+# **Conférencier**
+* Devoxx
+* Mix IT`,
     theme: 'light'
   },
   DEFAULT_CONTENT: [
-    `--expertise-start
+    `
+# Expérience
 
---expertise-archive
-
-JAVA / JEE, EJB 3.2, Servlet
-
---expertise-leaf
-
-Spring (Core, Batch, Boot), CDI, XML, XSL, XSD, JUnit, Jenkins, Jrules, Hibernate, Struts, Velocity
-
---expertise-end
-
-
-# Dernière expérience
+[Use one section per experience item. A section MUST not span more that one page.]::
+[You are responsible for page breaks. Use --break-page between sections insert visually nice page breaks. Check the page layout by using the print function of the browser (remove margins).]::
 
 --section-start
 
-# Consultant
-## BNP Paribas CIB- Equipe Feeds&Recs
-### Mai 2016 - Juin 2016
+[This is a placeholder section. See the next section for a more real life example.]::
 
-* Projet Feeds, application gérant
-  * Les Alimentations inter-applicatives Middle Office/Back Office
-  * Les descentes des flux financiers du Front vers le Back (Murex/ Ubix / Calypso…)
-  * Les Réconciliations Clearers/Front Office et Middle Office/Back Office : Paramétrage/ Reporting des réconciliations via Intellimatch (SUNGARD) et Business Object
-  * (Équipe de 7 personnes)
+# Job title
+## Company
+### Time period
 
-* Mission et tâches principales :
-  * La maintenance et l’amélioration des EAI/ESB qui régissent les flux de données afin d’intégrer un ensemble d’applications hétérogènes
-  * Amélioration des outils de test existants
-  * Proposer, concevoir et développer de nouveaux outils de tests
-  * Conception et développement des outils de reportings
-  * Analyse, conception et réalisation d’un PoC pour un nouvel EAI
+* Bullet point 1
+  * Sub-bullet point 1
+  * Sub-bullet point 2
 
-* **Environnement technique : JAVA 8/6, JEE, Spring 3/4 (Core, Boot, Batch), Maven (2/3), Jrules,Tibco BW (5.7), JUnit, Weblogic 9/11/12, XML, XSD, oracle 10g, CDI, Lombok, GIT, SVN, Jenkins,EJB 3.2**
+* Bullet point 2
+  * Sub-bullet point 1
+  * Sub-bullet point 2
 
 --section-end
 
 --break-page
-
---section-start
-
-# Consultant
-## BNP Paribas CIB- Equipe Feeds&Recs
-### Mai 2016 - Juin 2016
-
-* Projet Feeds, application gérant
-  * Les Alimentations inter-applicatives Middle Office/Back Office
-  * Les descentes des flux financiers du Front vers le Back (Murex/ Ubix / Calypso…)
-  * Les Réconciliations Clearers/Front Office et Middle Office/Back Office : Paramétrage/ Reporting des réconciliations via Intellimatch (SUNGARD) et Business Object
-  * (Équipe de 7 personnes)
-
-* Mission et tâches principales :
-  * La maintenance et l’amélioration des EAI/ESB qui régissent les flux de données afin d’intégrer un ensemble d’applications hétérogènes
-  * Amélioration des outils de test existants
-  * Proposer, concevoir et développer de nouveaux outils de tests
-  * Conception et développement des outils de reportings
-  * Analyse, conception et réalisation d’un PoC pour un nouvel EAI
-
-* **Environnement technique : JAVA 8/6, JEE, Spring 3/4 (Core, Boot, Batch), Maven (2/3), Jrules,Tibco BW (5.7), JUnit, Weblogic 9/11/12, XML, XSD, oracle 10g, CDI, Lombok, GIT, SVN, Jenkins,EJB 3.2**
-
---section-end
 
 --section-start
 
